2cfcb73d1abc134c540430e55c3a872acaac4961
hir0o <mk.9.q.kyu@gmail.com>
2023-04-29 15:39:33 +0900

    add dist

---
+++ dist/app.js
@@ -0,0 +1,11 @@
+(() => {
+  // src/styles.css.ts
+  var container = "styles_container__r80fof0";
+
+  // src/app.ts
+  document.write(`
+  <section class="${container}">
+    ...
+  </section>
+`);
+})();
